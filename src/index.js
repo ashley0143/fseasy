b9d7bc2bef79a3ae1b297a3437bf9fb665c1f583
aloima <83824840+aloima@users.noreply.github.com>
2021-07-11 15:22:51 +0300

    fix: fixed some methods with names, changed syntax

--- src/index.js
+++ src/index.js
@@ -1,45 +1,43 @@
 const fs = require("fs")
-const path = require("path")
+
 module.exports = class sj {
-    constructor(){
-        
+  readFile(file, property) {
+    if (!file) {
+      throw new Error("Which file do you wanna read?")
     }
-    readFile(file, property){
-        if(!file) {
-            throw new Error("Which file do you wanna read?")
-        }
-        if(!property){
-            return fs.readFileSync(file, "utf8")
-            }else{
-                return JSON.parse(fs.readFileSync(file, "utf8"))[property]
-            }
-        }
-    writeFile(file, text){
-        if(!file){
-            throw new Error("Which file do you wanna write?")
-        }
-        if(!text){
-            throw new Error("Blank text cannot be written")
-        }else{
-        try{
-        fs.writeFileSync(file, text, "utf8")
-    }catch(error){
-        throw Error(error)
+
+    if (!property) {
+      return fs.readFileSync(file, "utf8")
+    } else {
+      return JSON.parse(fs.readFileSync(file, "utf8"))[property]
     }
+  }
+
+  writeFile(file, text) {
+    if (!file) {
+      throw new Error("Which file do you wanna write?")
     }
+
+    if (!text) {
+      throw new Error("Blank text cannot be written")
+    } else {
+      fs.writeFile(file, text, (err) => {
+        if (err) throw err
+      })
     }
-    addFile(file, text){
-        if(!file){
-            throw new Error("Which file do you wanna write?")
-        }
-        if(!text){
-            throw new Error("Blank text cannot be written")
-        }else{
-        try{
-        fs.writeFileSync(file, fs.readFileSync(file, "utf8") + text, "utf8")
-    }catch(error){
-        throw Error(error)
+  }
+
+  appendFile(file, text) {
+    if (!file) {
+      throw new Error("Which file do you wanna write?")
     }
+
+    if (!text) {
+      throw new Error("Blank text cannot be written")
+    } else {
+      fs.appendFile(file, text, (err) => {
+        if (err) throw err
+      })
     }
-    }
+  }
 }
