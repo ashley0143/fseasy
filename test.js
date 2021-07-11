1691b1bd5fa9ca243803854201a148288b9db708
Falsis <63756985+falsisdev@users.noreply.github.com>
2021-07-11 18:46:14 +0300

    edit: Edited test.js

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ const savajcript = require("./src/index.js");
 const sj = new savajcript()
 let a = sj.writeFile("./test/testfile.json", `{\n"Testa": "Testb"\n}`)
 console.log(sj.readFile("./test/testfile.json", "Testa"))
-let b = sj.addFile("./test/testfile.json", `,{\n"Testv": "Testf"\n}`)
+let b = sj.appendFile("./test/testfile.json", `,{\n"Testv": "Testf"\n}`)
 console.log(sj.readFile("./test/testfile.json"))
