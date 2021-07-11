467c028fd5c7f354c3419caef8c830239c0bdb55
Falsis <63756985+falsisdev@users.noreply.github.com>
2021-07-11 14:25:10 +0300

    add: added writeFile() function

--- src/index.js
+++ src/index.js
@@ -14,4 +14,18 @@ module.exports = class sj {
                 return JSON.parse(fs.readFileSync(file, "utf8"))[property]
             }
         }
+    writeFile(file, text){
+        if(!file){
+            throw new Error("Which file do you wanna write?")
+        }
+        if(!text){
+            throw new Error("Blank text cannot be written")
+        }else{
+        try{
+        fs.writeFileSync(file, text, "utf8")
+    }catch(error){
+        throw Error(error)
+    }
+    }
+    }
 }
