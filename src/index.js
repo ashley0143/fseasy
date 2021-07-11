7c2b3f1bb39d9dee4e7839446017424aa409a589
Falsis <63756985+falsisdev@users.noreply.github.com>
2021-07-11 14:14:49 +0300

    edit: added property for readFile()

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ module.exports = class sj {
     constructor(){
         
     }
-    readFile(file){
+    readFile(file, property){
         if(!file) {
             throw new Error("Which file do you wanna read?")
         }
+        if(!property){
             return fs.readFileSync(file, "utf8")
+            }else{
+                return JSON.parse(fs.readFileSync(file, "utf8"))[property]
             }
+        }
 }
