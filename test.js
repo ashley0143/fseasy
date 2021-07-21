54853a67b87ae3b588af8ccf5b84df3c8e6d4ccd
Falsis <63756985+falsisdev@users.noreply.github.com>
2021-07-21 23:58:03 +0300

    edit: edited test file

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-const savajcript = require("./src/index.js");
-const sj = new savajcript()
-let a = sj.writeFile("./test/testfile.json", `{\n"Testa": "Testb"\n}`)
-console.log(sj.readFile("./test/testfile.json", "Testa"))
-let b = sj.appendFile("./test/testfile.json", `,{\n"Testv": "Testf"\n}`)
-console.log(sj.readFile("./test/testfile.json"))
+const fseasy = require("./src/index.js");
+const fs = new fseasy()
+let a = fs.writeFile("./test/testfile.json", `{\n"Testa": "Testb"\n}`)
+console.log(fs.readFile("./test/testfile.json", "Testa"))
+let b = fs.appendFile("./test/testfile.json", `,{\n"Testv": "Testf"\n}`)
+console.log(fs.readFile("./test/testfile.json"))
