8d4cc2b45cd456b68592b5ebc8eb876bdb6d2f3d
Demir <67432156+demir09@users.noreply.github.com>
2021-07-23 11:02:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,44 +4,45 @@ const event = require("events")
 class fseasy extends event {
   readFile(file, property) {
     if (!file) {
-      throw new Error("Which file do you wanna read?")
+      throw new Error("FsEasy Error: File is not provided.")
     }
 
     if (!property) {
        fs.readFileSync(file, "utf8")
+       this.emit("readFile" , {file: file, property: property ? property : null})
     } else {
        JSON.parse(fs.readFileSync(file, "utf8"))[property]
-       this.emit("readFile" , {file: file, property: property === "" ? null : property})
+       this.emit("readFile" , {file: file, property: property ? property : null})
     }
   }
 
   writeFile(file, text) {
     if (!file) {
-      throw new Error("Which file do you wanna write?")
+      throw new Error("FsEasy Error: File is not provided.")
     }
 
     if (!text) {
-      throw new Error("Blank text cannot be written")
+      throw new Error("FsEasy Error: Blank text cannot be written.")
     } else {
       fs.writeFile(file, text, (err) => {
-        if (err) throw err
+        if (err) throw err;
+        this.emit("writeFile" , {file: file, content: text})
       })
-      this.emit("writeFile" , {file: file, content: text})
     }
   }
 
   appendFile(file, text) {
     if (!file) {
-      throw new Error("Which file do you wanna write?")
+      throw new Error("FsEasy Error: File is not defined.")
     }
 
     if (!text) {
-      throw new Error("Blank text cannot be written")
+      throw new Error("FsEasy Error: Blank text cannot be written.")
     } else {
       fs.appendFile(file, text, (err) => {
-        if (err) throw err
+        if (err) throw err;
+        this.emit("appendFile" , { file: file, content: text})
       })
-      this.emit("appendFile" , { file: file, content: text})
     }
   }
 
