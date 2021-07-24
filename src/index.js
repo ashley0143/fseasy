b56ba4cfb516712aec4754fa84a2b3f7ff715b43
Falsis <keremtokul@hotmail.com>
2021-07-24 14:15:31 +0300

    fix: fixed an error

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ try{
 }
   
   jsonWrite(file , data , value) {
-    const dosya = JSON.parse(fs.readFileSync('./db/userdb.json' , 'utf-8'))
+    const dosya = JSON.parse(fs.readFileSync(file , 'utf-8'))
     dosya[data] = value
     return fs.writeFileSync(dosya , JSON.stringify(file, value, 2), "utf-8")
     this.emit("jsonWrite" , {file:file , data:data , value:value})
